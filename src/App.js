5f2ba834e573bd7e84a5948cd7f61dff596bdf0d
kinsibashir <kinsibashir@gmail.com>
2026-02-28 04:47:49 -0800

    Add App.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import ProjectList from "./components/ProjectList";
+
+function App() {
+  const projects = [
+    { id: 1, name: "Project Alpha", technologies: ["React", "CSS", "JS"] },
+    { id: 2, name: "Project Beta", technologies: ["HTML", "CSS"] },
+    { id: 3, name: "Project Gamma", technologies: ["Node", "Express", "MongoDB"] },
+  ];
+
+  return (
+    <div>
+      <NavBar />
+      <h1>My Projects</h1>
+      <ProjectList projects={projects} />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
